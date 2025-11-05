a0ed6aa2c41af52d5fef05115b78a7b88714de4d
Vipul Mehra <vipulmehra002@gmail.com>
2025-11-05 19:58:18 +0530

    update the code

--- src/main/java/com/paxaris/identity_service/identityService.java
+++ src/main/java/com/paxaris/identity_service/identityService.java
@@ -1,9 +1,12 @@
 	package com.paxaris.identity_service;
 
+	import com.paxaris.identity_service.dto.KeycloakConfig;
 	import org.springframework.boot.SpringApplication;
 	import org.springframework.boot.autoconfigure.SpringBootApplication;
+	import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 	@SpringBootApplication
+	@EnableConfigurationProperties(KeycloakConfig.class)
 		public class identityService {
 
 		public static void main(String[] args) {
